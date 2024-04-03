8a6d2317c807922b985f6e8012b17dd9337520c8
justin <justinmwathi32@gmail.com>
2024-04-03 13:25:20 +0300

    Lab completed

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Add your code here
+function submitData(name,email){
+return(fetch("http://localhost:3000/users",{
+    method:"POST",
+    headers:{
+        "Content-type":"application/json",
+        "Accept":"application/json",
+    },
+    body:JSON.stringify({
+        name:"Steve",
+        email:"steve@steve.com"
+    })
+})
+.then((response)=>response.json())
+.then((data)=>{
+    const newId=data.id;
+    appendNewIdToDOM(newId)
+})
+.catch(error => {
+    const message = `Error: ${error.message}`;
+    appendToDOM(message);
+  })
+);
+}
+
+const appendNewIdToDOM = (newId) => {
+    const newIdElement = document.createElement('p'); 
+    newIdElement.textContent = `New id: ${newId}`; 
+    document.body.appendChild(newIdElement); 
+  }
+  function appendToDOM(message) {
+    const div = document.createElement("div");
+    div.textContent = message;
+    document.body.appendChild(div); 
+  }
+submitData()
+ 
